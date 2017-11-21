0dfbe8afb91e6e0ef31368dcc01444580f405390
Eugene Krysanov <eugene.krysanov@gmail.com>
2017-11-21 17:40:34 +0500

    Employee  - form field translations have refactored

--- src/AppBundle/Form/EmployeeFormType.php
+++ src/AppBundle/Form/EmployeeFormType.php
@@ -31,31 +31,15 @@ class EmployeeFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname', TextType::class, [
-                'label' => 'employee.form.lastname.label',
-                'attr' => [
-                    'placeholder' => 'Введите фамилию сотрудника',
-                ],
-            ])
-            ->add('firstname', TextType::class, [
-                'label' => 'Имя',
-                'attr' => [
-                    'placeholder' => 'Введите имя сотрудника',
-                ],
-            ])
-            ->add('middlename', TextType::class, [
-                'label' => 'Отчество',
-                'attr' => [
-                    'placeholder' => 'Введите отчество сотрудника',
-                ],
-            ])
+            ->add('lastname', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('middlename', TextType::class)
             ->add('position', EntityType::class, [
                 'class' => Position::class,
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('p')
                         ->orderBy('p.id', 'ASC');
                 },
-                'label' => 'Должность',
             ])
             ->add('rank', EntityType::class, [
                 'class' => Rank::class,
@@ -63,22 +47,16 @@ class EmployeeFormType extends AbstractType
                     return $er->createQueryBuilder('r')
                         ->orderBy('r.id', 'ASC');
                 },
-                'label' => 'Звание',
             ])
             ->add('shift', EntityType::class, [
                 'class' => Shift::class,
-                'label' => 'Караул',
             ])
             /*->add('image', FileType::class, [
                 'required' => false,
                 'label' => 'Фотография',
             ])*/
             ->add('id', HiddenType::class)
-            ->add('submit', SubmitType::class, [
-                'attr' => [
-                    'class' => 'btn btn-lg btn-success btn-block',
-                ],
-            ]);
+            ->add('submit', SubmitType::class);
     }
 
     /**
@@ -88,6 +66,7 @@ class EmployeeFormType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => Employee::class,
+            'translation_domain' => 'admin',
         ));
     }
 
